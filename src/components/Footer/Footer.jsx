7e8ba940da47dfa96d982b044f7bc9c8141a7e38
mdaleardc <ismailaliamot@gmail.com>
2024-12-21 17:26:12 +0600

    removed development sign

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,4 @@
 import { FaSquareFacebook, FaLinkedin, FaSquareXTwitter } from "react-icons/fa6";
-import developmentSign from "../../assets/under_development.png"
 
 const Footer = () => {
   
@@ -9,9 +8,7 @@ const Footer = () => {
     <>
     
     <footer className="bg-black pt-[4rem] mx-auto">
-    <img src={developmentSign} alt="development sign" className='w-2/5 h-[200px] mx-auto'/>
     <div className='w-[95%] sm:w-5/6 text-white mx-auto grid grid-cols-2 sm:grid-cols-4 justify-center gap-x-2 gap-y-4 px-2 py-6'>
-    
     <div className=''>
     <h4 className='text-2xl font-medium text-yellow-400 tracking-wider'>Menu</h4>
     <p>Home</p>
